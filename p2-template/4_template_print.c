6d2aee87417c90bff59785dbd8a2731a4913fde5
Yashas Rao <71545509+yashasrao05@users.noreply.github.com>
2023-11-28 05:22:40 +0000

    p1 and p2

--- p2-template/4_template_print.c
+++ p2-template/4_template_print.c
@@ -3,27 +3,44 @@
 #define MAX_STRING   200
 
 typedef struct {
-    // Following members needed in struct:
-    // - name: string
-    // - roll number: string
-    // - marks: integer
-    // - grade: char
-
+    char name[20];
+    char rollno[20];
+    int marks;
+    char grade;
 } student_t;
 
 void print_student(student_t s) {
-    // Output should look like the following:
-    // Nitin C (abc123) got grade D with 50 marks
-
-    // Fill in the correct info for the "_" values and the "..."
-    printf("%_ (%_) for grade %_ with %_ marks\n", ...);
+    printf("%s (%s) got grade %c with %d marks\n", s.name, s.rollno, s.grade, s.marks);
 }
 
 void compute_grade(student_t* sptr) {
-    if (sptr->marks >= 90) sptr->grade = 'S';
-    else if 
-    else if
-    else ;
+    if (sptr->marks >= 90){
+        sptr->grade = 'S';
+    }
+
+    else if ((sptr->marks >= 80) && (sptr->marks < 90)){
+        sptr->grade = 'A';
+    }
+    
+    else if ((sptr->marks >= 70) && (sptr->marks < 80)){
+        sptr->grade = 'B';
+    }
+
+    else if ((sptr->marks >= 60) && (sptr->marks < 70)){
+        sptr->grade = 'C';
+    }
+
+    else if ((sptr->marks >= 50) && (sptr->marks < 60)){
+        sptr->grade = 'D';
+    }
+
+    else if ((sptr->marks >= 40) && (sptr->marks < 50)){
+        sptr->grade = 'E';
+    }
+
+    else{
+        sptr->grade = 'U';
+    }
 }
 
 int read_csv(char* filename, student_t s_array[MAX_STUDENTS]) {
@@ -34,11 +51,11 @@ int read_csv(char* filename, student_t s_array[MAX_STUDENTS]) {
     char buf[MAX_STRING];
     while (fgets(buf, MAX_STRING, fp) != NULL) {
         student_t* sptr = &s_array[num];
-        sscanf(buf, "%[^,],%[^,],%d", &(sptr->name), &(sptr->rollno), &(sptr->marks));
+        sscanf(buf, "%19[^,],%19[^,],%d", sptr->name, sptr->rollno, &(sptr->marks));
         compute_grade(sptr);
         num ++;
     }
-    return ...; 
+    return num; 
 }
 
 // Create a filename rollno.txt (example: abc123.txt) and write the template there
@@ -55,18 +72,18 @@ void tmpl_print(student_t s) {
 "The Dean\n";
 
     char filename[MAX_STRING];
-    sprintf(filename, "%s.txt", ...); 
+    sprintf(filename, "%s.txt", s.rollno); 
     FILE* fp = fopen(filename, "w");
 
-    fprintf(fp, tmpl, ...);
+    fprintf(fp, tmpl, s.name, s.rollno, s.name, s.marks, s.grade);
 }
 
 int main() {
     student_t s_array[MAX_STUDENTS];
-    
+
     int num = read_csv("students.csv", s_array);
 
-    for () {
-        tmpl_print(s);
+    for (int i=0; i < num; i++) {
+        tmpl_print(s_array[i]);
     }
 }
